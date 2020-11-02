502a8e9ef86575525f96852857dce179eb649eab
payoneercashout <56314279+payoneercashout@users.noreply.github.com>
2020-11-02 13:07:24 +0200

    3.5: Phonebook backend step5

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -38,6 +38,33 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+const generateId = () => {
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const person = {
+    name: body.name,
+	number: body.number,
+    id: generateId(),
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
 app.get('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
   const person = persons.find(person => person.id === id)
